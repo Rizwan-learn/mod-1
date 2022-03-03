941c1bee81822446a1673ce72a55b3d480ba8070
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-03 16:57:15 +0000

    create js with validate attribute

---
+++ validate.js
@@ -0,0 +1,21 @@
+
+const form = document.getElementById('form');
+function validation(e) {
+  const email = document.getElementById('email').value;
+
+  const text = document.getElementById('text');
+  const pattern = /^([a-z0-9._]+)@([a-z0-9])+.([a-z]+)(.[a-z]+)?$/;
+
+  if (email.match(pattern)) {
+    form.classList.add('valid');
+    form.classList.remove('invalid');
+    text.innerHTML = 'Your Email Address is valid';
+    text.style.color = 'green';
+  } else {
+    text.innerHTML = 'Enter valid Email using lowercase letter only';
+    text.style.color = 'red';
+    e.preventDefault();
+  }
+}
+
+form.addEventListener('submit', validation);
