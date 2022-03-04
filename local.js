d048f4290dbdd9712a91339aca154603d5025bfa
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-04 01:32:12 +0000

    allow auto save

--- local.js
+++ local.js
@@ -36,19 +36,27 @@ saveButton.onclick = (event) => {
   }
 
   form.addEventListener('submit', validation);
-  const data = getFormData();
-  localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
 };
 
-const populateForm = () => {
-  if (localStorage.key(formIdentifier)) {
-    const savedData = JSON.parse(localStorage.getItem(formIdentifier));
-    for (const element of formElements) {
-      if (element.name in savedData) {
-        element.value = savedData[element.name];
-      }
-    }
-  }
-};
+const contactForm = document.querySelector("#form");
+const fullName = document.querySelector("#name");
+const emailAddress = document.querySelector("#email");
+const message = document.querySelector("#textArea");
 
-document.onload = populateForm();
+function dataStorage() {
+  const user = {
+    userFullName: fullName.value,
+    userEmail: emailAddress.value,
+    userMessage: message.value,
+  };
+
+  localStorage.setItem("userData", JSON.stringify(user));
+}
+
+contactForm.addEventListener("focusout", dataStorage);
+
+const userDataObject = JSON.parse(localStorage.getItem("userData"));
+
+fullName.value = userDataObject.userFullName;
+emailAddress.value = userDataObject.userEmail;
+message.value = userDataObject.userMessage;
